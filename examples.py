5cd99ce3cf0723f3d25cdf3e558fac128a12863a
b1art <70599221+b1art@users.noreply.github.com>
2022-06-25 20:01:44 +0300

    upd

--- examples.py
+++ examples.py
@@ -2,6 +2,9 @@ import solver
 
 def f(x):
     return x**3 + x**2 - 1.23
+def df(x):
+    return 2*x**2 + 2*x
 
 print(solver.bisection(f, 0, 1, 0.0001))
-
+print(solver.chord(f, 0, 1, 0.0001))
+print(solver.newton(f, df, 1, 0.0001))
--- setup.c
+++ setup.c
@@ -3,11 +3,12 @@
 #include <math.h>
 
 static PyObject *
-bisection(PyObject *self, PyObject *args)
+bisection(PyObject *self, PyObject *args)   //метод бисекции, дихотомии, деления отрезка пополам
 {
     PyObject *cb1;
 	double eps, x_n, x_k, tmp, x_i;
 	double res1, res2;
+	int steps=0;
 
     if (!PyArg_ParseTuple(args, "Oddd", &cb1, &x_n, &x_k, &eps))
         return 0;
@@ -39,9 +40,76 @@ bisection(PyObject *self, PyObject *args)
         }
         else {
         x_n = x_i;
+        steps++;
         }
 	}
-	return Py_BuildValue("d", x_i);
+	return Py_BuildValue("db", x_i, steps);
+}
+
+static PyObject *
+chord(PyObject *self, PyObject *args)   //метод хорд, секущих
+{
+    PyObject *cb1;
+	double eps, x_next, x_curr;
+	double res1, res2;
+	int steps=0;
+
+    if (!PyArg_ParseTuple(args, "Oddd", &cb1, &x_curr, &x_next, &eps))
+        return 0;
+
+    if (!PyCallable_Check(cb1)) {
+        PyErr_SetString(PyExc_TypeError, "chord: a callable is required");
+        return 0;
+    }
+
+	PyObject *arg = Py_BuildValue("(d)", x_curr);
+	res1 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg));
+	PyObject *arg1 = Py_BuildValue("(d)", x_next);
+	res2 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg1));
+	while(fabs(x_next - x_curr) > eps)
+	{
+        x_curr = x_next - (x_next - x_curr) * res2 / (res2 - res1);
+        PyObject *arg = Py_BuildValue("(d)", x_curr);
+        res1 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg));
+
+        x_next = x_curr - (x_curr - x_next) * res1 / (res1 - res2);
+        PyObject *arg1 = Py_BuildValue("(d)", x_next);
+        res2 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg1));
+        steps++;
+	}
+	return Py_BuildValue("db", x_next,steps);
+}
+
+static PyObject *
+newton(PyObject *self, PyObject *args)  //метод Ньютона
+{
+    PyObject *cb1, *cb2;
+	double eps, xn, x1, x0;
+	double res1, res2;
+	int steps=0;
+
+    if (!PyArg_ParseTuple(args, "OOdd", &cb1, &cb2, &xn, &eps))
+        return 0;
+
+    if (!PyCallable_Check(cb1) || !PyCallable_Check(cb2)) {
+        PyErr_SetString(PyExc_TypeError, "newton: a callable is required");
+        return 0;
+    }
+
+	PyObject *arg = Py_BuildValue("(d)", xn);
+	res1 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg));
+	res2 = PyFloat_AsDouble(PyObject_CallObject(cb2, arg));
+	x1 = xn - res1/res2;
+	x0 = xn;
+	while (fabs (x0 - x1) > eps) {
+		arg = Py_BuildValue("(d)", x1);
+		res1 = PyFloat_AsDouble(PyObject_CallObject(cb1, arg));
+		res2 = PyFloat_AsDouble(PyObject_CallObject(cb2, arg));
+		x0 = x1;
+		x1 = x1 - res1/res2;
+		steps++;
+	}
+	return Py_BuildValue("db", x1,steps);
 }
 
 
@@ -51,6 +119,18 @@ static PyMethodDef ownmod_methods[] = {
         bisection,
         METH_VARARGS,
         "bisection function"
+    },
+    {
+        "chord",
+        chord,
+        METH_VARARGS,
+        "chord function"
+    },
+    {
+        "newton",
+        newton,
+        METH_VARARGS,
+        "newton function"
     },
     { NULL, NULL, 0, NULL }
 };
